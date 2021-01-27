82092ef4bc45ebe35c5f52792a641cfd15449b1b
Micraow <15190450708@163.com>
2021-01-27 14:11:20 +0800

    deamon

--- APIhandler.py
+++ APIhandler.py
@@ -2,7 +2,7 @@ import tokens
 import requests
 import json
 import threading
-
+import time
 
 token = tokens.AUTH("66f884d4-0c48-48eb-8753-43524e18830f",
                     "7vSsDNnV6i2snIp~2BDQ_-cc.jb-IPR2O4")  # 此时会自动检查有无登陆。
@@ -17,8 +17,13 @@ class APIs:
         }
 
     def check_token(self):
-        ''' # 载入自动刷新token的线程.'''
+        '''  载入自动刷新token的线程.'''
         token.refresh_acc_tk()
-        TokenreFresher = threading.Timer(
-            3585, token.refresh_acc_tk)  # 每过3599秒，access token会过期,所以获取令牌后3585秒就刷新
+        TokenreFresher = threading.Thread(target=self.refresh_timer,daemon=True)  # 每过3599秒，access token会过期,所以获取令牌后3585秒就刷新
         TokenreFresher.start()
+
+    def refresh_timer(self):
+        while True:
+            token.refresh_acc_tk()
+            time.sleep(3585)
+
